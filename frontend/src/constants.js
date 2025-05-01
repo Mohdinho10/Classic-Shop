5ce91baad01567c8740c0dc68770a719984aefd4
muhammadqulatain123@gmail.com <Mohdinho10>
2025-05-01 23:33:26 +0300

    Adding the url of the hosting platform

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,8 @@
 export const BASE_URL =
   // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://classic-shop.onrender.com";
 export const PRODUCTS_URL = "/api/products";
 export const CART_URL = "/api/cart";
 export const USERS_URL = "/api/users";
